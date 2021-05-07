e1375ae4af5cc2e5a6ef2973b11b9e460226e809
gonzs <gonzalosisnero@gmail.com>
2021-05-07 19:25:30 -0300

    Add voters to the contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -14,6 +14,9 @@ contract Election {
     // Store Candidates Count
     uint256 public candidatesCount;
 
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+
     constructor() public {
         addCandidate("Candidate-1");
         addCandidate("Candidate-2");
@@ -22,5 +25,19 @@ contract Election {
     function addCandidate(string memory _name) private {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint256 _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount++;
     }
 }
